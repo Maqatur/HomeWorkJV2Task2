3a8d88d97d9ed13600323e0775cb88befbb47b78
Maqatur <maqaturs@gmail.com>
2024-02-01 22:09:18 +0400

    fixed

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        int refill = 1000;
+        int refill = 100;
         int bonus = refill / 100;
         int balance = refill + bonus;
 
-        if (refill >= 1000) {
+        if (refill > 1000) {
             balance = refill + bonus;
         } else {
             balance = refill;
         }
+        if (bonus < 10) {
+            bonus = 0;
+        }
 
         System.out.println("Вы пополнили счет. Ваш баланс составляет: " + balance + " рублей");
+        System.out.println("Вам начисленно бонусов: " + bonus);
 
     }
 }
